700537603f2e7e04538b3b9f16177a1ab4d62198
Javi S. Planelles <javiersanchez@casfid.com>
2024-10-15 16:48:41 +0200

    feat: add types and counter buttons

--- client/pages/product.tsx
+++ client/pages/product.tsx
@@ -1,17 +1,31 @@
 import { gql } from '@apollo/client';
+import { useState } from 'react';
 import client from '../apolloClient';
+import { Header } from '../components/Header';
+import { ProductImage } from '../components/ProductImage';
 
-const PRODUCT_QUERY = gql`
+export const PRODUCT_QUERY = gql`
   query PRODUCT_QUERY($id: ID!) {
     Product(id: $id) {
       id
       name
+      power
       description
+      price
+      quantity
+      brand
+      weight
+      height
+      width
+      length
+      model_code
+      colour
+      img_url
     }
   }
 `;
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
   const { data } = await client.query({
     query: PRODUCT_QUERY,
     variables: { id: 1 },
@@ -19,11 +33,55 @@ export async function getServerSideProps() {
 
   return {
     props: {
-      product: data,
+      product: data.Product,
     },
   };
 }
 
-export default function Product({ product }) {
-  return <div>{product.Product.name}</div>;
+interface Props {
+  product: {
+    id: number;
+    name: string;
+    power: string;
+    description: string;
+    price: number;
+    quantity: number;
+    brand: string;
+    weight: number;
+    height: number;
+    width: number;
+    length: number;
+    model_code: string;
+    colour: string;
+    img_url: string;
+  };
+}
+
+export default function Product({ product }: Props) {
+  const [productQuantity, setProductQuantity] = useState(1);
+
+  const handleIncreaseQuantity = () =>
+    setProductQuantity((prevState) => prevState + 1);
+
+  const handleReduceQuantity = () =>
+    setProductQuantity((prevState) => prevState - 1);
+
+  return (
+    <>
+      <Header />
+      <ProductImage imageUrl={product.img_url} />
+      <h1> {product.name}</h1>
+      <p> {product.power}</p>
+      <p> {product.quantity}</p>
+      {product.price}GBP
+      <div>
+        <button onClick={handleReduceQuantity} disabled={productQuantity === 1}>
+          -
+        </button>
+        <span title='Current quantity'>{productQuantity}</span>
+        <button onClick={handleIncreaseQuantity}>+</button>
+      </div>
+      <button>Add to cart</button>
+    </>
+  );
 }
