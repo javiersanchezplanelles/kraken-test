574a7d72fea2b783ce8704841abc34946d89aff2
Javi S. Planelles <javiersanchez@casfid.com>
2024-10-15 16:49:21 +0200

    feat: update test

--- client/test/product.test.tsx
+++ client/test/product.test.tsx
@@ -1,39 +1,47 @@
-import { render, fireEvent } from "@testing-library/react";
-import Product from "../pages/product";
+import { MockedProvider } from '@apollo/client/testing';
+import { fireEvent, render, screen } from '@testing-library/react';
+import Product from '../pages/product';
+import { apolloMocks, productMock } from './mocks/product.mock';
 
-test("should be able to increase and decrease product quantity", async () => {
-  const { getByText, getByTitle } = render(<Product />);
+describe('Product page', () => {
+  beforeEach(() => {
+    render(
+      <MockedProvider mocks={apolloMocks} addTypename={false}>
+        <Product product={productMock} />
+      </MockedProvider>
+    );
+  });
 
-  const increaseQuantity = getByText("+");
+  test('should be able to increase and decrease product quantity', async () => {
+    const increaseQuantity = screen.getByText('+');
 
-  const currentQuantity = getByTitle("Current quantity");
-  expect(currentQuantity).toHaveTextContent("1");
+    const currentQuantity = screen.getByTitle('Current quantity');
+    expect(currentQuantity).toHaveTextContent('1');
 
-  fireEvent.click(increaseQuantity);
-  expect(currentQuantity).toHaveTextContent("2");
+    fireEvent.click(increaseQuantity);
+    expect(currentQuantity).toHaveTextContent('2');
 
-  const decreaseQuantity = getByText("-");
+    const decreaseQuantity = screen.getByText('-');
 
-  fireEvent.click(decreaseQuantity);
-  expect(currentQuantity).toHaveTextContent("1");
-});
+    fireEvent.click(decreaseQuantity);
+    expect(currentQuantity).toHaveTextContent('1');
+  });
 
-test("should be able to add items to the basket", async () => {
-  const { getByText, getByTitle } = render(<Product />);
+  test('should be able to add items to the basket', async () => {
+    const increaseQuantity = screen.getByText('+');
 
-  const increaseQuantity = getByText("+");
+    const currentQuantity = screen.getByTitle('Current quantity');
 
-  const currentQuantity = getByTitle("Current quantity");
+    fireEvent.click(increaseQuantity);
+    fireEvent.click(increaseQuantity);
+    fireEvent.click(increaseQuantity);
 
-  fireEvent.click(increaseQuantity);
-  fireEvent.click(increaseQuantity);
-  fireEvent.click(increaseQuantity);
+    expect(currentQuantity).toHaveTextContent('4');
 
-  expect(currentQuantity).toHaveTextContent("4");
+    const addToBasketElement = screen.getByText('Add to cart');
+    fireEvent.click(addToBasketElement);
 
-  const addToBasketElement = getByText("Add to cart");
-  fireEvent.click(addToBasketElement);
-
-  const basketItems = getByTitle("Basket items");
-  expect(basketItems).toHaveTextContent("4");
+    const basketItems = screen.getByTitle('Basket items');
+    expect(basketItems).toHaveTextContent('4');
+  });
 });
