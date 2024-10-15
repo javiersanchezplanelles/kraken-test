d47721e25b35a8f89a905e3e8f7d63395a1c6184
Javi S. Planelles <javiersanchez@casfid.com>
2024-10-15 08:16:51 +0200

    feat: add graphql query

--- client/pages/product.tsx
+++ client/pages/product.tsx
@@ -1,3 +1,29 @@
-export default function Product() {
-  return <div>Product page</div>;
+import { gql } from '@apollo/client';
+import client from '../apolloClient';
+
+const PRODUCT_QUERY = gql`
+  query PRODUCT_QUERY($id: ID!) {
+    Product(id: $id) {
+      id
+      name
+      description
+    }
+  }
+`;
+
+export async function getServerSideProps() {
+  const { data } = await client.query({
+    query: PRODUCT_QUERY,
+    variables: { id: 1 },
+  });
+
+  return {
+    props: {
+      product: data,
+    },
+  };
+}
+
+export default function Product({ product }) {
+  return <div>{product.Product.name}</div>;
 }
