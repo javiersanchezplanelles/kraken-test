3488e2e3b0e1fb50d97e9495e04bb810c2322e77
Javi S. Planelles <javiersanchez@casfid.com>
2024-10-15 22:11:55 +0200

    feat: update header

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -4,10 +4,26 @@ import styled from 'styled-components';
 const HeaderWrapper = styled.header`
   display: flex;
   justify-content: space-between;
+  align-items: center;
   max-height: 80px;
   height: 100%;
 `;
 
+const CartWrapper = styled.div`
+  position: relative;
+`;
+
+const ProductQuantity = styled.div`
+  background-color: var(--ice);
+  color: var(--siphon);
+  border-radius: 50%;
+  width: 20px;
+  height: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+`;
 interface Props {
   productsOnCart: number;
 }
@@ -16,8 +32,14 @@ export const Header = ({ productsOnCart }: Props) => {
   return (
     <HeaderWrapper>
       <Image src='/octopus-logo.svg' width={150} height={150} />
-      <span title='Basket items'>{productsOnCart}</span>
-      <Image src='/basket.svg' width={30} height={30} />
+      <CartWrapper>
+        {productsOnCart && (
+          <ProductQuantity title='Basket items'>
+            {productsOnCart}
+          </ProductQuantity>
+        )}
+        <Image src='/basket.svg' width={30} height={30} />
+      </CartWrapper>
     </HeaderWrapper>
   );
 };
