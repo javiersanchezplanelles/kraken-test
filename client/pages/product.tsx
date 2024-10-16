6f5449ae59207a0b3402c226e51f1c06f00e9fc5
Javi S. Planelles <javiersanchez@casfid.com>
2024-10-16 20:18:37 +0200

    feat: update product page and global styles

--- client/pages/product.tsx
+++ client/pages/product.tsx
@@ -1,8 +1,15 @@
 import { gql } from '@apollo/client';
 import { useState } from 'react';
 import client from '../apolloClient';
+import { ContentBlock } from '../components/ContentBlock';
+import { Footer } from '../components/Footer';
 import { Header } from '../components/Header';
 import { ProductDetail } from '../components/ProductDetail';
+import { ProductSpecifications } from '../components/ProductSpecifications';
+import type {
+  ProductQueryResponse,
+  Product as ProductType,
+} from '../domain/product.types';
 
 export const PRODUCT_QUERY = gql`
   query PRODUCT_QUERY($id: ID!) {
@@ -26,7 +33,7 @@ export const PRODUCT_QUERY = gql`
 `;
 
 export async function getStaticProps() {
-  const { data } = await client.query({
+  const { data }: { data: ProductQueryResponse } = await client.query({
     query: PRODUCT_QUERY,
     variables: { id: 1 },
   });
@@ -39,26 +46,12 @@ export async function getStaticProps() {
 }
 
 interface Props {
-  product: {
-    id: number;
-    name: string;
-    power: string;
-    description: string;
-    price: number;
-    quantity: number;
-    brand: string;
-    weight: number;
-    height: number;
-    width: number;
-    length: number;
-    model_code: string;
-    colour: string;
-    img_url: string;
-  };
+  product: ProductType;
 }
 
 export default function Product({ product }: Props) {
   const [productsOnCart, setProductsOnCart] = useState<number | null>(null);
+  const { height, width, length, colour, brand, weight, model_code } = product;
 
   return (
     <>
@@ -67,6 +60,11 @@ export default function Product({ product }: Props) {
         product={product}
         updateProductsOnCart={setProductsOnCart}
       />
+      <ContentBlock heading='Description' content={product.description} />
+      <ProductSpecifications
+        content={{ height, width, length, colour, brand, weight, model_code }}
+      />
+      <Footer />
     </>
   );
 }
