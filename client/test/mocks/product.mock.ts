0a2e5c78da024d5abd51982bffac5aa3dc5e30d4
Javi S. Planelles <javiersanchez@casfid.com>
2024-10-16 20:18:02 +0200

    feat: update product mock

--- client/test/mocks/product.mock.ts
+++ client/test/mocks/product.mock.ts
@@ -1,5 +1,26 @@
 import { PRODUCT_QUERY } from '../../pages/product';
 
+const specificationsMock = {
+  brand: 'Philips',
+  weight: 77,
+  height: 12.6,
+  width: 6.2,
+  length: 6.2,
+  model_code: 'E27 ES',
+  colour: 'Cool daylight',
+};
+
+const productQueryMock = {
+  id: 1,
+  name: 'Energy saving light bulb',
+  power: '25W',
+  description:
+    'Available in 7 watts, 9 watts, 11 watts Spiral Light bulb in B22, bulb switches on instantly, no wait around warm start and flicker free features make for a great all purpose bulb',
+  price: 1299,
+  quantity: 4,
+  img_url: 'https://i.ibb.co/2nzwxnQ/bulb.png',
+}
+
 export const productMock = {
   id: 1,
   name: 'Energy saving light bulb',
@@ -8,14 +29,8 @@ export const productMock = {
     'Available in 7 watts, 9 watts, 11 watts Spiral Light bulb in B22, bulb switches on instantly, no wait around warm start and flicker free features make for a great all purpose bulb',
   price: 1299,
   quantity: 4,
-  brand: 'Philips',
-  weight: 77,
-  height: 12.6,
-  width: 6.2,
-  length: 6.2,
-  model_code: 'E27 ES',
-  colour: 'Cool daylight',
   img_url: 'https://i.ibb.co/2nzwxnQ/bulb.png',
+  specifications: specificationsMock
 };
 
 export const apolloMocks = [
