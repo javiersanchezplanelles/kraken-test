728bf6ac8e349406dbf743147a86573a3daac88c
Javi S. Planelles <javiersanchez@casfid.com>
2024-10-17 20:44:21 +0200

    feat: tweak product page

--- client/pages/product.tsx
+++ client/pages/product.tsx
@@ -1,36 +1,14 @@
-import { gql } from '@apollo/client';
 import { useState } from 'react';
+import styled from 'styled-components';
 import client from '../apolloClient';
-import { ContentBlock } from '../components/ContentBlock';
 import { Footer } from '../components/Footer';
 import { Header } from '../components/Header';
 import { ProductDetail } from '../components/ProductDetail';
-import { ProductSpecifications } from '../components/ProductSpecifications';
 import type {
   ProductQueryResponse,
   Product as ProductType,
 } from '../domain/product.types';
-
-export const PRODUCT_QUERY = gql`
-  query PRODUCT_QUERY($id: ID!) {
-    Product(id: $id) {
-      id
-      name
-      power
-      description
-      price
-      quantity
-      brand
-      weight
-      height
-      width
-      length
-      model_code
-      colour
-      img_url
-    }
-  }
-`;
+import { PRODUCT_QUERY } from '../services/product.graphql';
 
 export async function getStaticProps() {
   const { data }: { data: ProductQueryResponse } = await client.query({
@@ -49,22 +27,23 @@ interface Props {
   product: ProductType;
 }
 
+const ProductWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`;
+
 export default function Product({ product }: Props) {
   const [productsOnCart, setProductsOnCart] = useState<number | null>(null);
-  const { height, width, length, colour, brand, weight, model_code } = product;
 
   return (
-    <>
+    <ProductWrapper>
       <Header productsOnCart={productsOnCart} />
       <ProductDetail
         product={product}
         updateProductsOnCart={setProductsOnCart}
       />
-      <ContentBlock heading='Description' content={product.description} />
-      <ProductSpecifications
-        content={{ height, width, length, colour, brand, weight, model_code }}
-      />
       <Footer />
-    </>
+    </ProductWrapper>
   );
 }
