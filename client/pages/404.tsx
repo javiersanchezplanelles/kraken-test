490fbed4b2ff6ef49d23fa064db7426365aa7c88
Javi S. Planelles <javiersanchez@casfid.com>
2024-10-19 11:03:45 +0200

    feat: style 404 page

--- client/pages/404.tsx
+++ client/pages/404.tsx
@@ -1,11 +1,26 @@
+import styled from 'styled-components';
 import { CtaButton } from '../components/CtaButton';
 import { Layout } from '../components/Layout';
 import { APP_ROUTES } from '../domain/routes/routes.constants';
 
+const Custom404Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 20px;
+  text-align: center;
+
+  a {
+    max-width: 200px;
+  }
+`;
+
 const Custom404 = () => (
   <Layout>
-    <h1>Oops, we could not find what you were looking for</h1>
-    <CtaButton text='Back home' href={APP_ROUTES.HOME} />
+    <Custom404Wrapper>
+      <h1>Oops, we could not find what you were looking for</h1>
+      <CtaButton text='Back home' href={APP_ROUTES.HOME} />
+    </Custom404Wrapper>
   </Layout>
 );
 
