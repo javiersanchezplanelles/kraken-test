3bc2d7188cef2abd2710dbe568e747360eda3d6a
Javi S. Planelles <javiersanchez@casfid.com>
2025-05-18 18:46:12 +0200

    feat: make index the product page

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,11 +1,65 @@
-import Link from 'next/link';
+import { ApolloQueryResult } from '@apollo/client';
+import Head from 'next/head';
+import { useState } from 'react';
+import apolloClient from '../apolloClient';
+import { Layout } from '../components/Layout';
+import { ProductDetail } from '../components/ProductDetail';
+import { ProductReview } from '../components/ProductReview';
+import type {
+  ProductQueryResponse,
+  Product as ProductType,
+} from '../domain/product/product.types';
+import { PRODUCT_QUERY } from '../services/product.graphql';
 
-export default function Home() {
+export async function getStaticProps() {
+  try {
+    const { data, error }: ApolloQueryResult<ProductQueryResponse> =
+      await apolloClient.query({
+        query: PRODUCT_QUERY,
+        variables: { id: 1 },
+      });
+
+    if (error) {
+      return {
+        notFound: true,
+      };
+    }
+
+    if (!data || !data.Product) {
+      return {
+        notFound: true,
+      };
+    }
+
+    return {
+      props: {
+        product: data.Product,
+      },
+    };
+  } catch (error) {
+    return {
+      notFound: true,
+    };
+  }
+}
+
+interface Props {
+  product: ProductType;
+}
+
+export default function Product({ product }: Props) {
+  const [productsOnBasket, setProductsOnBasket] = useState<number>(0);
+
   return (
-    <main>
-      <div className='home'>
-        <Link href='./product'>Product page here</Link>
-      </div>
-    </main>
+    <Layout productsOnBasket={productsOnBasket}>
+      <Head>
+        <title>{product.name}</title>
+      </Head>
+      <ProductDetail
+        product={product}
+        updateProductsOnBasket={setProductsOnBasket}
+      />
+      <ProductReview />
+    </Layout>
   );
 }
